1aa24bc86c15e429b9124ae2415797f6bb2fb855
willy <williman.nicolas@gmail.com>
2021-05-02 19:40:02 -0300

    metodo para registrar un rescate de mascota

--- src/main/java/dominio/sistema/Registro.java
+++ src/main/java/dominio/sistema/Registro.java
@@ -2,14 +2,17 @@ package dominio.sistema;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import dominio.mascota.Mascota;
+import dominio.personas.Persona;
+import dominio.ubicacion.Coordenadas;
 import dominio.usuarios.Usuario;
 
 public class Registro {
   private List<Usuario> dueniosRegistrados = new ArrayList<Usuario>();
   private List<Mascota> mascotasRegistradas = new ArrayList<Mascota>();
-  // private List<EncuentroDeMascota> = new ArrayList<EncuentroDeMascota>();
+  private List<Rescate> rescates = new ArrayList<Rescate>();
 
   public void registrarDuenio(Usuario duenio) {
     this.dueniosRegistrados.add(duenio);
@@ -17,5 +20,26 @@ public class Registro {
 
   public void registrarMascota(Mascota mascota) {
     this.mascotasRegistradas.add(mascota);
+  }
+
+  public void confirmarMascotaEncontrada(Persona persona, int idMascota, List<String> fotos, String descripcion, Coordenadas lugar) {
+    // TODO validar que ningun campo sea null
+    if (fotos.size() < 1) {
+      throw new RuntimeException("La cantidad de fotos debe ser como minimo 1."); // TODO ver si necesitamos crear una excepcion custom
+    }
+
+    Mascota mascota = this.mascotasRegistradas
+                          .stream()
+                          .filter(m -> m.getId() == idMascota)
+                          .collect(Collectors.toList())
+                          .get(0);
+
+    if (mascota == null) {
+      throw new RuntimeException("Mascota no registrada");
+    }
+
+    Rescate rescate = new Rescate(persona, mascota, fotos, descripcion, lugar);
+
+    this.rescates.add(rescate);
   }
 }
