15c4581100d2bebdca4bb4d29353d0121b147f98
Emmanuel <emmanuel.lazarte.012@gmail.com>
2021-05-03 20:43:08 -0300

    Corregimos problema de indentación

--- src/test/java/PersonaTest.java
+++ src/test/java/PersonaTest.java
@@ -5,41 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-/*
-import main.java.dominio.personas.Persona;
-import main.java.dominio.personas.TipoDeDocumento;
-import main.java.dominio.personas.Contacto;
-import main.java.dominio.usuarios.*;
-*/
 class PersonaTest {
 
 	@Test
 	void elNombreDeSusanaNoEsSusano() {
 		Persona susana = this.SusanaPersona();
-		
+
 		assertFalse(susana.getNombre().equals("Susano"));
 	}
-	
+
 	@Test
 	void elContactoDeSusanaEsSusanita() {
 		Persona susana = this.SusanaPersona();
-	
+
 		assertEquals("Susanita", susana.getContacto().getNombre());
 	}
 
 	//
 	private Contacto SusanitaContacto() {
-		return new Contacto(
-				"Susanita", "Gimenez",
-				47732311, "susanita@gmail.com"
-				);
+		return new Contacto("Susanita", "Gimenez", 47732311, "susanita@gmail.com");
 	}
-	
+
 	private Persona SusanaPersona() {
-		return new Persona(
-				"Susana", "Gimenez",
-				TipoDeDocumento.DNI, 25216110,
-				this.SusanitaContacto()
-				);
+		return new Persona("Susana", "Gimenez", TipoDeDocumento.DNI, 25216110, this.SusanitaContacto());
 	}
 }
