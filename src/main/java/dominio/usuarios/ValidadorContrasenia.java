c70077f68ed2263a8ae4ebadc537b43f0ac1ed52
Emmanuel <emmanuel.lazarte.012@gmail.com>
2021-05-04 17:00:16 -0300

    Implementamos validacion de claves inseguras y sus respectivas pruebas

--- src/main/java/dominio/usuarios/ValidadorContrasenia.java
+++ src/main/java/dominio/usuarios/ValidadorContrasenia.java
@@ -1,8 +1,31 @@
 package dominio.usuarios;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class ValidadorContrasenia {
-  boolean validarContraseña(){
-    return true;
-    //TODO
+	private String fileName;
+	
+	public ValidadorContrasenia() {
+		this.fileName = FileSystems.getDefault().getPath("10k-most-common.txt").toString();
+	}
+  
+  public boolean esVulnerableLaContrasenia(String contrasenia) {
+    List<String> list = new ArrayList<>();
+
+    try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+    	return stream.anyMatch(elemento -> elemento.contentEquals(contrasenia));
+        		
+    } catch (IOException e) {
+        e.printStackTrace();
+    }  	
+    
+    return false;
   }
 }
---
+++ src/test/java/ValidadorContraseniaTest.java
@@ -0,0 +1,55 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import dominio.usuarios.ValidadorContrasenia;
+
+class ValidadorContraseniaTest {
+	ValidadorContrasenia validadorContrasenia;
+	
+	@BeforeEach
+	void setup() {
+		validadorContrasenia = new ValidadorContrasenia();		
+	}
+	
+	@Test
+	void noEsUnaClaveSeguraSoloPonerNumeros() {		
+		assertTrue(validadorContrasenia.esVulnerableLaContrasenia("123456"));
+		assertTrue(validadorContrasenia.esVulnerableLaContrasenia("123456789"));
+	}
+	
+	@Test
+	void noEsUnaClaveSeguraUsarNombresDeComics() {		
+		assertTrue(validadorContrasenia.esVulnerableLaContrasenia("batman"));
+		assertTrue(validadorContrasenia.esVulnerableLaContrasenia("iceman"));
+		assertTrue(validadorContrasenia.esVulnerableLaContrasenia("superman"));
+	}
+	
+	@Test
+	void noEsUnaClaveSeguraUsarNombreDeColores() {		
+		assertTrue(validadorContrasenia.esVulnerableLaContrasenia("orange"));
+		assertTrue(validadorContrasenia.esVulnerableLaContrasenia("black"));
+	}
+
+	@Test
+	void noEsUnaClaveSeguraUsarNombreDePersonas() {		
+		assertTrue(validadorContrasenia.esVulnerableLaContrasenia("andrea"));
+		assertTrue(validadorContrasenia.esVulnerableLaContrasenia("thomas"));
+	}
+
+	@Test
+	void esUnaClaveSeguraUsarFrases() {		
+		assertFalse(validadorContrasenia.esVulnerableLaContrasenia("cuestionesdelavida"));
+		assertFalse(validadorContrasenia.esVulnerableLaContrasenia("unaensaladafria"));
+		assertFalse(validadorContrasenia.esVulnerableLaContrasenia("undiadeveranoquiendiria"));
+	}
+
+	@Test
+	void esSeguraUnaClaveAlfanumericaConSimbolos() {		
+		assertFalse(validadorContrasenia.esVulnerableLaContrasenia("123asd123.0?"));
+	}
+
+}
