1c3fcf325d3ced31c37fadc438c584d75246b240
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-05-04 14:09:28 -0300

    Ahora el Rescate tiene un rescatista y las fotos se agregan de una

--- src/main/java/dominio/sistema/Rescate.java
+++ src/main/java/dominio/sistema/Rescate.java
@@ -1,40 +1,40 @@
 package dominio.sistema;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import dominio.Rescatista;
 import dominio.mascota.Mascota;
-import dominio.personas.Persona;
 import dominio.ubicacion.Coordenadas;
 import java.time.temporal.ChronoUnit;
 
 public class Rescate {
-  private Persona persona; // TODO chequear si esto no deberia ser Rescatista
+  private Rescatista rescatista; // TODO chequear si esto no deberia ser Rescatista
   private Mascota mascota;
   private List<String> fotos;
   private String descripcion;
   private Coordenadas lugar;
   private LocalDate fecha;
 
-  public Rescate(Persona persona, Mascota mascota, String descripcion,LocalDate fecha) {
-    this.persona = persona;
+  public Rescate(Rescatista rescatista, Mascota mascota, String descripcion, LocalDate fecha) {
+    this.rescatista = rescatista;
     this.mascota = mascota;
     this.descripcion = descripcion;
     this.fecha = fecha;
+    this.fotos = new ArrayList<String>();
   }
 
-  public void setFotos(List<String> fotos) {
-    this.fotos = fotos;
+  public void agregarUnaFoto(String url){
+    fotos.add(url);
   }
-
 
   public void setLugar(Coordenadas lugar) {
     this.lugar = lugar;
   }
 
-
   public Boolean sucedioDentroDeLosUltimos10Dias() {
-    return Math.abs(ChronoUnit.DAYS.between(LocalDate.now(),fecha)) <= 10 ;
+    return Math.abs(ChronoUnit.DAYS.between(LocalDate.now(), fecha)) <= 10;
   }
 
   public Mascota getMascota() {
