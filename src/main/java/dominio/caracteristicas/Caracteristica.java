6b597d592f59a27ea2ebbeb82c74f11f9f42cf5e
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-05-04 21:46:21 -0300

    Ahora característica usa Lista

--- src/main/java/dominio/caracteristicas/Caracteristica.java
+++ src/main/java/dominio/caracteristicas/Caracteristica.java
@@ -1,15 +1,15 @@
 package dominio.caracteristicas;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import dominio.util.Lista;
 
 public class Caracteristica {
 	private String titulo;
-	private List<String> opciones;
+	private Lista<String> opciones;
 	
 	public Caracteristica(String titulo) { 
 		this.titulo = titulo.toUpperCase();
-		this.opciones = new ArrayList<String>();
+		this.opciones = new Lista<String>();
 	}
 
 	public void agregarOpcion(String opcion) {
@@ -24,7 +24,7 @@ public class Caracteristica {
 		return opciones.contains(opcion.toUpperCase());
 	}
 
-	public List<String> opciones(){
+	public Lista<String> opciones(){
 		return opciones;
 	}
 }
