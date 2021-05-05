10d032c1a126f952ff04eeaf6b37dcdfa1a1d991
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-05-04 21:45:34 -0300

    Elimino imports innecesarios

--- src/test/java/RescateTest.java
+++ src/test/java/RescateTest.java
@@ -1,9 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Formatter;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
