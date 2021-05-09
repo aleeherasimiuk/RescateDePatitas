aadf385e91fe04eeaf10991a82afd5bb1393ae52
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-05-08 22:52:41 -0300

    Test más lindo, alee mas feliz

--- src/test/java/CaracteristicaTest.java
+++ src/test/java/CaracteristicaTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 class CaracteristicaTest {
 	RepositorioCaracteristicas repoCaracteristica = RepositorioCaracteristicas.getINSTANCE();
@@ -39,12 +40,13 @@ class CaracteristicaTest {
 
 	@Test
 	void alAgregarUnaCaracteristicaConElMismoNombreDeUnaExistenteRompe() {
+
+		String expected = "Ya existe una caracteristica con ese titulo. Verifique si se trata de un error o intente con otro titulo";
 		Administrador administrador = new Administrador("UnAdministrador", "holaqtaltodomuyBarat10");
-		Exception exception = assertThrows(RuntimeException.class,
-				() -> administrador.agregarUnaCaracteristica("castrado", "SI", "NO", "NO SE"));
-		assertEquals(
-				"Ya existe una caracteristica con ese titulo. Verifique si se trata de un error o intente con otro titulo",
-				exception.getMessage());
+		Executable agregarCaracteristica = () -> administrador.agregarUnaCaracteristica("castrado", "SI", "NO", "NO SE");
+
+		Exception exception = assertThrows(RuntimeException.class, agregarCaracteristica);
+		assertEquals(expected,exception.getMessage());
 	}
 
 }
