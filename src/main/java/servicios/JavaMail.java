1d44deb92109cfbf3d415ca3271a195fe831b7b0
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-06-01 22:20:57 -0300

    Ahora se ignoran los bugs que trae consigo retrofit

--- src/main/java/servicios/JavaMail.java
+++ src/main/java/servicios/JavaMail.java
@@ -1,7 +1,6 @@
 package servicios;
 
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
--- src/test/java/Fixture.java
+++ src/test/java/Fixture.java
@@ -1,6 +1,5 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 
 import dominio.asociacion.Asociacion;
 import dominio.mascota.ClaseMascota;
