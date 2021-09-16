74775cc2a40ecada81f312b0bb7ff2565ec6273f
willy <williman.nicolas@gmail.com>
2021-09-16 20:03:18 -0300

    modificado entorno para que funque con docker, y actualizados comanditos en el run.bash

--- run.bash
+++ run.bash
@@ -1,16 +1,28 @@
 #!/bin/bash
-commands=('install' 'test')
 
 case $1 in
   install)
     docker exec -it rescate_de_patitas_app mvn -Duser.home=/var/maven dependency:resolve
     ;;
-
   test)
     docker exec -it rescate_de_patitas_app mvn -Duser.home=/var/maven test
     ;;
+  package)
+    docker exec -it rescate_de_patitas_app mvn -Duser.home=/var/maven package
+    ;;
+  run)
+    docker exec -it rescate_de_patitas_app java -jar /home/app/target/RescatePatitas-jar-with-dependencies.jar
+    ;;
+  clean)
+    docker exec -it rescate_de_patitas_app mvn -Duser.home=/var/maven clean
+    ;;
+  all)
+    docker exec -it rescate_de_patitas_app mvn -Duser.home=/var/maven clean
+    docker exec -it rescate_de_patitas_app mvn -Duser.home=/var/maven package
+    docker exec -it rescate_de_patitas_app java -jar /home/app/target/RescatePatitas-jar-with-dependencies.jar
+    ;;
   *)
-    echo 'Wrong parameter, valid parameters are: install, test'
+    echo 'Wrong parameter, valid parameters are: install, test, package, run, all'
     exit 1
     ;;
 esac
