2f16a6db54dc30f6ec14fd60bd51109a21560bac
willy <williman.nicolas@gmail.com>
2021-09-16 21:50:13 -0300

    removed tests from mvn package in run script

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ case $1 in
     ;;
   all)
     docker exec -it rescate_de_patitas_app mvn -Duser.home=/var/maven clean
-    docker exec -it rescate_de_patitas_app mvn -Duser.home=/var/maven package
+    docker exec -it rescate_de_patitas_app mvn -Duser.home=/var/maven -Dmaven.test.skip package
     docker exec -it rescate_de_patitas_app java -jar /home/app/target/RescatePatitas-jar-with-dependencies.jar
     ;;
   *)
