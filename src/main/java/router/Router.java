c774ec23dda9429a0c580d493132ba5ee2c4f521
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-10-20 14:32:23 -0300

    Add pet form in two columns

--- src/main/java/router/Router.java
+++ src/main/java/router/Router.java
@@ -2,18 +2,24 @@ package router;
 
 import controllers.HomeController;
 import controllers.LoginController;
+import dominio.mascota.Caracteristica;
 import dominio.repositorio.RepositorioCaracteristicas;
 
 import static spark.Spark.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
+
+import com.github.jknack.handlebars.Handlebars;
 
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class Router {
   public void setup() {
+    
     HandlebarsTemplateEngine engineTemplate = new HandlebarsTemplateEngine();
 
 		staticFiles.location("public");
@@ -38,7 +44,9 @@ public class Router {
     get("/blah", (request, response) -> {
       
       Map<String, Object> model = new HashMap<>();
-      model.put("characteristics", RepositorioCaracteristicas.getINSTANCE().todos());
+      List<Caracteristica> caracteristicas = RepositorioCaracteristicas.getINSTANCE().todos();
+      model.put("characteristicsEven", caracteristicas.subList(0, (caracteristicas.size() + 1) / 2));
+      model.put("characteristicsOdd", caracteristicas.subList((caracteristicas.size() + 1) / 2, (caracteristicas.size())));
       return new ModelAndView(model, "add_pet.hbs");
     } , engineTemplate);
   }
