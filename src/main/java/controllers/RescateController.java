8e821b14ea07d7ebc78acdca1fccb0bc5ccd85a5
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-10-29 20:58:55 -0300

    Testing navbar

--- src/main/java/controllers/RescateController.java
+++ src/main/java/controllers/RescateController.java
@@ -1,8 +1,11 @@
 package controllers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import dominio.mascota.Caracteristica;
+import dominio.repositorio.RepositorioCaracteristicas;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -13,6 +16,10 @@ public class RescateController {
   public static ModelAndView view(Request request, Response response){
     Map<String, Object> model = new HashMap<>();
     model.put("error", false);
+    List<Caracteristica> caracteristicas = RepositorioCaracteristicas.getINSTANCE().todos();
+    model.put("characteristicsEven", caracteristicas.subList(0, (caracteristicas.size() + 1) / 2));
+    model.put("characteristicsOdd",
+        caracteristicas.subList((caracteristicas.size() + 1) / 2, (caracteristicas.size())));
     return new ModelAndView(model, "rescuer.hbs");
   }
   
